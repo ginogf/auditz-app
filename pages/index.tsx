6651a247342cb7274ae548b88ea49e4db35a398b
Gino <gagf50@gmail.com>
2023-02-12 22:51:36 -0500

    Responsivness fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,32 +9,32 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-    const [userInput, setUserInput] = useState('');
-    const [apiOutput, setApiOutput] = useState('')
-    const [isGenerating, setIsGenerating] = useState(false)
-  
-    const callGenerateEndpoint = async () => {
-      setIsGenerating(true);
-      
-      console.log("Calling OpenAI...")
-      const response = await fetch('/api/generate', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ userInput }),
-      });
-  
-      const data = await response.json();
-      const { output } = data;
-      console.log("OpenAI replied...", output.text)
-  
-      setApiOutput(`${output.text}`);
-      setIsGenerating(false);
-    }
-    const onUserChangedText = (event: { target: { value: SetStateAction<string>; }; }) => {
-      setUserInput(event.target.value);
-    };
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  const onUserChangedText = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setUserInput(event.target.value);
+  };
 
   return (
     <>
@@ -53,41 +53,41 @@ export default function Home() {
           <div className="header-subtitle">
             <h2>efortlessly secure your contracts in minutes, for free.</h2>
           </div>
-          <br/>
+          <br />
+        </div>
+        <div className="prompt-container">
           <div>
             <p>Copy your contract here:</p>
           </div>
-          <div className="prompt-container">
-            <textarea 
-              className="prompt-box"
-              placeholder="start typing here"
-              value={userInput}
-              onChange={onUserChangedText}
-            />
-            <div className="prompt-buttons">
-              <a
-                className={isGenerating ? 'generate-button loading' : 'generate-button'}
-                onClick={callGenerateEndpoint}
-              >
-                <div className="generate">
+          <textarea
+            className="prompt-box"
+            placeholder="start typing here"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
                 {isGenerating ? <span className="loader"></span> : <p>Generate report</p>}
-                </div>
-              </a>
-            </div>
-          {apiOutput && (
-            <div className="output">
-              <div className="output-header-container">
-                <div className="output-header">
-                  <h3>Report</h3>
-                </div>
               </div>
-              <div className="output-content">
-                <p>{apiOutput}</p>
+            </a>
+          </div>
+        </div>
+        {apiOutput && (
+          <div className="report-container">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Report</h3>
               </div>
             </div>
-          )}
-        </div>
-        </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
       </div>
     </>
   )
